78df78042d74f73201cf6a13f5a3bc42ca58e760
Emil Söderlind <emil.soderlind@svt.se>
2024-11-08 09:31:37 +0100

    Adding swagger

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,9 +3,13 @@ import database from "./services/eventService";
 import express from "express";
 import eventController from "./controllers/eventController";
 import cors from "cors";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app = express();
 const port = process.env.PORT;
+
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 if (!port) {
   throw new Error("PORT is not defined");
@@ -19,6 +23,8 @@ database.connectToDatabase();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/event", eventController);
 
